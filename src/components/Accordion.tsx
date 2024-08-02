9a7dd9ad1c41e1608473b3ef17c0dd2473c7c412
Jonatas <oliveirajonatasfaria@gmail.com>
2024-08-02 18:08:37 -0300

    fix errors on propostas

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,7 +4,11 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-import { GraduationCap, HeartHandshake, PencilRuler, ShieldCheck } from "lucide-react"
+import { GraduationCap, HandHeart, HeartHandshake, PencilRuler, ShieldCheck } from "lucide-react"
+import { FaRoad } from "react-icons/fa";
+import { FaShieldDog } from "react-icons/fa6";
+import { Button } from "./ui/button";
+import { IoIosMore } from "react-icons/io";
 
 export function AccordionPropostas()
 {
@@ -12,43 +16,60 @@ export function AccordionPropostas()
         <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
                 
-                <AccordionTrigger><div className="flex felx-row gap-2"><GraduationCap className="block" />Educação de Qualidade</div></AccordionTrigger>
+                <AccordionTrigger><div className="flex flex-row gap-2 items-center"><FaShieldDog width="60" height="60" className="block w-24 scale-150" />Criação de Centros de Apoio e Cuidados para Animais de Rua e de Famílias
+                    de Baixa Renda</div></AccordionTrigger>
                 <AccordionContent>
-                    Comprometemo-nos a lutar por uma educação
-                    pública de excelência. Participamos de iniciativas para melhorar a
-                    infraestrutura escolar e apoiar estudantes em situação de
-                    vulnerabilidade. No futuro, pretendemos intensificar investimentos
-                    na capacitação de professores e programas de apoio ao aluno.
+                    A proposta para a criação de Centros de Apoio e Cuidados para Animais de Rua e de Famílias de Baixa Renda visa atender às necessidades de saúde e bem-estar animal, além de auxiliar famílias de baixa renda a cuidar de seus pets. Os centros oferecerão pontos de alimentação e água para animais de rua, atendimento veterinário gratuito, exames laboratoriais, tratamentos de saúde, programas de castração, e apoio à adoção responsável. A iniciativa também incluirá campanhas educativas para conscientização sobre maus-tratos e a criação de um canal de denúncia. Através de parcerias com ONGs e empresas, os centros serão sustentáveis e eficientes, promovendo uma cidade mais justa e solidária com os direitos dos animais.
+                    <a href="/propostas/animaisderua">
+                        <Button
+                            variant="outline"
+                            className="flex flex-row justify-center items-center bg-trasnparent m-2"
+                        ><IoIosMore className="mr-2 h-6 w-6 text-white"
+                            />Saiba mais</Button>
+                    </a>
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-                <AccordionTrigger><div className="flex felx-row gap-2"><HeartHandshake className="block" />Saúde para Todos</div></AccordionTrigger>
+                <AccordionTrigger><div className="flex flex-row gap-2 items-center">
+                    <FaRoad width="24" height="24" className="block w-24" />Proposta para Pavimentação de Vias e Melhoria do Saneamento Básico em
+                    Comunidades</div></AccordionTrigger>
                 <AccordionContent>
-                     Com um histórico de apoio a ações comunitárias de
-                    saúde, queremos melhorar o acesso aos serviços de saúde pública.
-                    Planejamos reduzir filas e garantir um atendimento humanizado e
-                    eficaz nas unidades de saúde, baseando-nos em nossa experiência
-                    prática e conhecimento das necessidades locais.
+                    A proposta para Pavimentação de Vias e Melhoria do Saneamento Básico em Comunidades busca enfrentar problemas de infraestrutura que afetam a saúde, mobilidade e segurança dos moradores. O projeto inclui o mapeamento das áreas mais necessitadas de pavimentação, execução de obras com materiais de alta qualidade, e implementação de sistemas de saneamento, como redes de esgoto e estações de tratamento. O objetivo é melhorar a qualidade de vida, reduzir doenças e acidentes, valorizar propriedades e preservar o meio ambiente. O financiamento será público, com possíveis parcerias, e a gestão será transparente e participativa, promovendo desenvolvimento social e econômico nas comunidades beneficiadas.
+                    <a href="/propostas/pavimentacao">
+                        <Button
+                            variant="outline"
+                            className="flex flex-row justify-center items-center bg-trasnparent m-2"
+                        ><IoIosMore className="mr-2 h-6 w-6 text-white"
+                            />Saiba mais</Button>
+                    </a>
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-                <AccordionTrigger><div className="flex felx-row gap-2"><ShieldCheck className="block" />Segurança e Bem-Estar</div></AccordionTrigger>
+                <AccordionTrigger><div className="flex flex-row gap-2 items-center"><HandHeart width="24" height="24" className="block w-24" />Proposta para a Criação de um Centro de Apoio Integral para Pessoas de
+                    Baixa Renda</div></AccordionTrigger>
                 <AccordionContent>
-                    Acreditamos em uma cidade mais segura e temos
-                    trabalhado em projetos de prevenção à violência e apoio à segurança
-                    pública. Pretendemos ampliar esses esforços, promovendo parcerias
-                    entre a comunidade e as forças de segurança para criar um ambiente
-                    mais seguro para todos.
+                    A proposta para a criação de um Centro de Apoio Integral para Pessoas de Baixa Renda visa fornecer serviços essenciais como assistência jurídica, suporte psicológico e orientação vocacional, com um enfoque especial em jovens. O centro, localizado em uma área central e acessível, oferecerá consultas jurídicas gratuitas, atendimento psicológico, educação e capacitação profissional. Contará com uma equipe multidisciplinar qualificada e será financiado por verbas públicas e parcerias. O objetivo é garantir acesso igualitário a serviços fundamentais, promover a inclusão social e fortalecer a comunidade, oferecendo às famílias de baixa renda as ferramentas necessárias para uma vida digna e segura.
+                    <a href="/propostas/centrodeapoio">
+                        <Button
+                            variant="outline"
+                            className="flex flex-row justify-center items-center bg-trasnparent m-2"
+                        ><IoIosMore className="mr-2 h-6 w-6 text-white"
+                            />Saiba mais</Button>
+                    </a>
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-4">
-                <AccordionTrigger><div className="flex felx-row gap-2"><PencilRuler  className="block" />Infraestrutura Urbana</div></AccordionTrigger>
+                <AccordionTrigger><div className="flex flex-row gap-2 items-center"><HeartHandshake width="24" height="24" className="block w-24" />Proposta para Implantação de uma Clínica de Reabilitação para
+                    Dependentes Químicos e Alcoólicos Custeada pelo Governo</div></AccordionTrigger>
                 <AccordionContent>
-                    Contribuímos para melhorias em
-                    infraestrutura, como pavimentação de ruas e iluminação pública.
-                    Nosso compromisso é continuar investindo em projetos que melhorem a
-                    qualidade de vida de todos os guarulhenses, criando uma cidade mais
-                    moderna e acessível
+                    A proposta para a implantação de uma Clínica de Reabilitação para Dependentes Químicos e Alcoólicos, totalmente custeada pelo governo, visa combater a dependência de drogas e álcool, oferecendo tratamento integral e suporte necessário para recuperação. A clínica será estrategicamente localizada e equipada com instalações modernas. Oferecerá desintoxicação segura, terapia individual e em grupo, programas de educação e capacitação para reintegração social, e suporte contínuo para os pacientes e suas famílias. Com uma equipe multidisciplinar qualificada, a clínica busca reduzir a criminalidade, melhorar a saúde pública e fortalecer a comunidade.
+                    <a href="/propostas/clinicadereabilitacao">
+                        <Button
+                            variant="outline"
+                            className="flex flex-row justify-center items-center bg-trasnparent m-2"
+                        ><IoIosMore className="mr-2 h-6 w-6 text-white"
+                            />Saiba mais</Button>
+                    </a>
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
